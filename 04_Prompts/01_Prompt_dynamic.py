c76ffe1d8ea80bc5c82c4eae15eb60b64f9992ca
Vraj Makwana <vrajm@zignuts.com>
2025-08-01 17:18:30 +0530

    Add Dynamic Prompt

---
+++ 04_Prompts/01_Prompt_dynamic.py
@@ -0,0 +1,29 @@
+from langchain_core.prompts import PromptTemplate
+from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
+
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen3-Coder-480B-A35B-Instruct",
+    task="text-generation",
+    max_new_tokens=100)
+
+model = ChatHuggingFace(llm=llm)
+
+templet = PromptTemplate(
+    template=  """
+    Summarize the following text into a short paragraph:
+    {text}
+    Summary:
+    """,
+    input_variables=['text']
+)
+
+input_Data = input("Enter Something Here : \n")
+
+# fill the placeholders
+prompt = templet.invoke({
+    "text" : input_Data
+})
+
+result = model.invoke(prompt)
+
+print(result.content)
